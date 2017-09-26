79e1d1be2e6c2bf37061e0c62066204639869ab4
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-09-26 17:39:41 +0000

    Finished interface with fake info to test

--- lib/weatherapp/cli.rb
+++ lib/weatherapp/cli.rb
@@ -3,12 +3,36 @@ class Weatherapp::CLI #CLI Controller
 def call
 
   puts "Today's Weather:"
-  show_weather
-
+  list_states
+  menu
+  goodbye
 end
 
-def show_weather
-  puts "The weather is sunny with a temperature of 98 degrees in Sarasota, FL"
+def list_states
+  puts "1. Alabama 2. New york ... etc"
 end
+
+def menu
+  "Please choose the state or type states to see the states again:"
+  input = nil
+  while input != "exit"
+    input = gets.strip.downcase
+    case input
+      when "1"
+       puts "weather for city 1"
+      when "2"
+       puts "weather for city 2"
+     when "states"
+       list_states
+     else 
+       puts "Not sure what you want, type states or exit"
+     end
+
+  end
+end
+
+  def goodbye
+      puts "See you tomorrow!"
+  end
 
 end
