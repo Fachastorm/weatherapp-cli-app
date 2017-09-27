b829cad5d8e0c8772c5b5aae491286786c7ed99f
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-09-27 23:14:13 +0000

    Added the list_cities method

--- lib/weatherapp/cli.rb
+++ lib/weatherapp/cli.rb
@@ -16,7 +16,7 @@ def greeting
 
 end
 
-def list_states
+  def list_states
   states = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming" ]
   puts "Please type the state that you'd like to see the weather for."
   puts "If you would like to see the list of states again please type states. Otherwise type exit to quit."
@@ -25,16 +25,26 @@ def list_states
      state = gets.strip.downcase
      if states.include?(state)
        @state_weather = Weatherapp::Weather.new(state)
-       @state_weather.city_weather
+       @state_weather.city_list
+       list_cities
      elsif state == "states"
        states.each.with_index(1) do |s, i|
          puts "#{i}. #{s.capitalize}"
        end
+       puts "Please type the state name that you would like to see the weather for."
      elsif state != "states" && state != "exit" && states.include?(state) == false
          puts "I don't know what you mean, please type states to see a list of states or exit."
        end
  end
 end
+
+  def list_cities
+    puts "You have chosen #{@state_weather.state}."
+    puts "These are the available weather stations in your selected state."
+      @state_weather.cities.each.with_index(1) do |c, i|
+        "puts #{i}. #{c.name}"
+      end
+    end
 
 
   def goodbye
--- lib/weatherapp/weather.rb
+++ lib/weatherapp/weather.rb
@@ -1,22 +1,23 @@
+require "pry"
+
 class Weatherapp::Weather
 
-  attr_accessor :name, :cities, :state, :url
+  attr_accessor :cities, :state, :url
 
   def initialize(state)
     @state = state
   end
 
-  def city_weather
+  def city_list
     html = open("https://www.wunderground.com/cgi-bin/findweather/getForecast?query=#{@state}")
     weather_list = Nokogiri::HTML(html)
-    all_cities = []
+    @cities = []
     weather_list.css("tbody tr").each do |w|
-       city = w.css("a").text
-       url = w.css("a").attr("href").value
-       all_cities << "#{city} : #{url}"
+       city.name = w.css("a").text
+       city.url = w.css("a").attr("href").value
+      @cities << "#{city.name} : #{city.url}"
     end
-    @cities = all_cities
+    @cities
   end
-
 
 end
